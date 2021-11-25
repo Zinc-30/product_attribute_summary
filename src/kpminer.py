759922d5b652272267878ae4e7915623a974ef40
Yubo Wang <50394396+Wyb0627@users.noreply.github.com>
2021-11-25 18:13:19 +0800

    Add files via upload

--- src/kpminer.py
+++ src/kpminer.py
@@ -1,12 +1,11 @@
 import string
+import pke
 import os
 import shutil
-import pke
 import json
 from pathlib import Path
 import tqdm
 from nltk.corpus import stopwords
-
 
 def mkdir(path):
     folder = os.path.exists(path)
@@ -14,28 +13,28 @@ def mkdir(path):
         os.makedirs(path)
 
 
-def tfidf(config, stoplist=list(string.punctuation)):
+def kpminer(config, stoplist=list(string.punctuation)):
     path = Path(config['data_dir'])
     path_out = Path(config['output_dir'])
     num = config['number']
-    extractor = pke.unsupervised.TfIdf()
+    extractor = pke.unsupervised.KPMiner()
     document_frequency = pke.load_document_frequency_file(str(path_out.joinpath('doc_freq.tsv.gz')))
     for file_name in tqdm.tqdm(config['doc_files']):
-        # for file_name in tqdm.tqdm(path.glob('*.txt')):
+    #for file_name in tqdm.tqdm(path.glob('*.txt')):
         extractor.load_document(input=str(path.joinpath(file_name)), language='en', normalization=None)
-        extractor.candidate_selection(n=3, stoplist=stoplist)
-        extractor.candidate_weighting(document_frequency)
+        extractor.candidate_selection(lasf=3, cutoff=400, stoplist=stoplist)
+        extractor.candidate_weighting(df=document_frequency, alpha=2.3, sigma=3.0)
         keyphrases = extractor.get_n_best(num, redundancy_removal=True)
-        mkdir(str(path_out.joinpath('tfidf/')))
-        with open(str(path_out.joinpath('tfidf/')) + '/{}.keywords'.format(file_name), 'w') as file:
+        mkdir(str(path_out.joinpath('kpminer/')))
+        with open(str(path_out.joinpath('kpminer/')) + '/{}.keywords'.format(file_name), 'w') as file:
             json.dump({file_name: keyphrases}, file)
-    shutil.rmtree(path_out.joinpath('tfidf_source/'))
+    shutil.rmtree(path_out.joinpath('kpminer_source/'))
 
 def caculate_freq(config, stoplist=list(string.punctuation)):
     data_dir = Path(config['data_dir'])
     output_dir = Path(config['output_dir'])
-    input_total = output_dir.joinpath('tfidf_source/')
-    out_freq = input_total.joinpath('doc_freq.tsv.gz')
+    input_total = output_dir.joinpath('kpminer_source/')
+    out_freq= input_total.joinpath('doc_freq.tsv.gz')
     mkdir(str(input_total))
     for file_name in config['doc_files']:
         shutil.copy(data_dir.joinpath(file_name), input_total)
@@ -53,6 +52,7 @@ if __name__ == '__main__':
     path_out = config['output_dir']
     stoplist = list(string.punctuation)
     caculate_freq(config, stoplist)
-    print('Using model: TFIDF')
-    tfidf(config, stoplist)
-    print('Finish model: TFIDF')
+    print('Using model: KPMINER')
+    kpminer(config, stoplist)
+    print('Finish model: KPMINER')
+
