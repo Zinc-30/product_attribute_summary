89ff073337b4e4c5b8b61259ce95e185487aeb52
xinhao <xinhao20122013@hotmail.com>
2021-12-14 16:21:31 +0800

    omit secret key

--- src/productSearch/productSearch/settings.py
+++ src/productSearch/productSearch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j#svkx5f%t1-b8(3xdg_fi(f(hvuvkbrh4=dmu*uwy3-#j##@0'
+# SECRET_KEY = 'django-insecure-j#svkx5f%t1-b8(3xdg_fi(f(hvuvkbrh4=dmu*uwy3-#j##@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
